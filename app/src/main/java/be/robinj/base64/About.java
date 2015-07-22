6749f07aea616cdc072173a24fc03cd2fca1d83a
= <robin@robinj.be>
2015-07-22 15:50:32 +0200

    Created a new project and pasted the old code into it because Gradle was throwing a hissy fit.

--- app/src/main/java/be/robinj/base64/About.java
+++ app/src/main/java/be/robinj/base64/About.java
@@ -16,32 +16,32 @@ import android.widget.TextView;
 
 public class About extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+				.add(R.id.container, new PlaceholderFragment())
+				.commit();
+		}
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.about, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        return super.onOptionsItemSelected(item);
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.about, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		return super.onOptionsItemSelected(item);
+	}
 
 	public void btnDev_clicked (View view)
 	{
@@ -49,37 +49,37 @@ public class About extends ActionBarActivity {
 		this.startActivity (intent);
 	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment()
-        {
-        }
+		public PlaceholderFragment()
+		{
+		}
 
-        @Override
-        public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            View rootView = inflater.inflate (R.layout.fragment_about, container, false);
+		@Override
+		public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			View rootView = inflater.inflate (R.layout.fragment_about, container, false);
 
-	        TextView version = (TextView) rootView.findViewById (R.id.version);
-	        String v = "";
+			TextView version = (TextView) rootView.findViewById (R.id.version);
+			String v = "";
 
-	        try
-	        {
-		        PackageInfo pkgInfo = this.getActivity ().getPackageManager ().getPackageInfo (this.getActivity ().getPackageName (), 0);
-		        v = "v" + pkgInfo.versionName;
-	        }
-	        catch (PackageManager.NameNotFoundException e)
-	        {
-		        v = "{" + e.toString () + "}";
-	        }
+			try
+			{
+				PackageInfo pkgInfo = this.getActivity ().getPackageManager ().getPackageInfo (this.getActivity ().getPackageName (), 0);
+				v = "v" + pkgInfo.versionName;
+			}
+			catch (PackageManager.NameNotFoundException e)
+			{
+				v = "{" + e.toString () + "}";
+			}
 
-	        version.setText (v);
+			version.setText (v);
 
-            return rootView;
-        }
-    }
+			return rootView;
+		}
+	}
 
 }
--- app/src/main/java/be/robinj/base64/MainActivity.java
+++ app/src/main/java/be/robinj/base64/MainActivity.java
@@ -16,44 +16,44 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView (R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+				.add(R.id.container, new PlaceholderFragment())
+				.commit();
+		}
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId ();
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-        if (id == R.id.action_about)
-        {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId ();
+
+		if (id == R.id.action_about)
+		{
 			Intent intent = new Intent (this, About.class);
-	        this.startActivity (intent);
+			this.startActivity (intent);
 
-            return true;
-        }
+			return true;
+		}
 
-        return super.onOptionsItemSelected(item);
-    }
+		return super.onOptionsItemSelected(item);
+	}
 
 	public static String encode (String str)
 	{
@@ -65,95 +65,95 @@ public class MainActivity extends ActionBarActivity {
 		return new String (Base64.decode (str.trim ().getBytes (), Base64.DEFAULT));
 	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment
-    {
-	    public PlaceholderFragment ()
-        {
-        }
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment
+	{
+		public PlaceholderFragment ()
+		{
+		}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-	        final TextView txtPlain = (TextView) rootView.findViewById (R.id.txtPlain);
-	        final TextView txtBase64 = (TextView) rootView.findViewById (R.id.txtBase64);
+			final TextView txtPlain = (TextView) rootView.findViewById (R.id.txtPlain);
+			final TextView txtBase64 = (TextView) rootView.findViewById (R.id.txtBase64);
 
-	        txtPlain.addTextChangedListener
-	        (
-		        new TextWatcher ()
-		        {
-			        @Override
-			        public void beforeTextChanged (CharSequence charSequence, int i, int i2, int i3)
-			        {
-				        txtPlain.setError (null);
-				        txtBase64.setError (null);
-			        }
+			txtPlain.addTextChangedListener
+				(
+					new TextWatcher ()
+					{
+						@Override
+						public void beforeTextChanged (CharSequence charSequence, int i, int i2, int i3)
+						{
+							txtPlain.setError (null);
+							txtBase64.setError (null);
+						}
 
-			        @Override
-			        public void onTextChanged (CharSequence charSequence, int i, int i2, int i3)
-			        {
-				        if (txtPlain.hasFocus ())
-				        {
-					        try
-					        {
-						        String encoded = MainActivity.encode(charSequence.toString());
-				                txtBase64.setText(encoded);
-					        }
-					        catch (IllegalArgumentException ex)
-					        {
-								txtBase64.setError (getString (R.string.invalidInput));
-					        }
-				        }
-			        }
+						@Override
+						public void onTextChanged (CharSequence charSequence, int i, int i2, int i3)
+						{
+							if (txtPlain.hasFocus ())
+							{
+								try
+								{
+									String encoded = MainActivity.encode(charSequence.toString());
+									txtBase64.setText(encoded);
+								}
+								catch (IllegalArgumentException ex)
+								{
+									txtBase64.setError (getString (R.string.invalidInput));
+								}
+							}
+						}
 
-			        @Override
-			        public void afterTextChanged (Editable editable)
-			        {
-			        }
-		        }
-	        );
+						@Override
+						public void afterTextChanged (Editable editable)
+						{
+						}
+					}
+				);
 
-	        txtBase64.addTextChangedListener
-            (
-		        new TextWatcher ()
-		        {
-			        @Override
-			        public void beforeTextChanged (CharSequence charSequence, int i, int i2, int i3)
-			        {
-				        txtPlain.setError (null);
-				        txtBase64.setError (null);
-			        }
+			txtBase64.addTextChangedListener
+				(
+					new TextWatcher ()
+					{
+						@Override
+						public void beforeTextChanged (CharSequence charSequence, int i, int i2, int i3)
+						{
+							txtPlain.setError (null);
+							txtBase64.setError (null);
+						}
 
-			        @Override
-			        public void onTextChanged (CharSequence charSequence, int i, int i2, int i3)
-			        {
-				        if (txtBase64.hasFocus ())
-				        {
-					        try
-					        {
-						        String decoded = MainActivity.decode(charSequence.toString());
-				                txtPlain.setText(decoded);
-					        }
-					        catch (IllegalArgumentException ex)
-					        {
-						        txtBase64.setError (getString (R.string.invalidBase64));
-					        }
-				        }
-			        }
+						@Override
+						public void onTextChanged (CharSequence charSequence, int i, int i2, int i3)
+						{
+							if (txtBase64.hasFocus ())
+							{
+								try
+								{
+									String decoded = MainActivity.decode(charSequence.toString());
+									txtPlain.setText(decoded);
+								}
+								catch (IllegalArgumentException ex)
+								{
+									txtBase64.setError (getString (R.string.invalidBase64));
+								}
+							}
+						}
 
-			        @Override
-			        public void afterTextChanged (Editable editable)
-			        {
-			        }
-		        }
-	        );
+						@Override
+						public void afterTextChanged (Editable editable)
+						{
+						}
+					}
+				);
 
-            return rootView;
-        }
-    }
+			return rootView;
+		}
+	}
 
 }
